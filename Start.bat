72499bbeaccab327916b69c2f145ec0b832d0389
Michael Quaschny <m.quaschny@dbuss.de>
2026-03-08 08:09:22 +0100

    Initial

---
+++ Start.bat
@@ -0,0 +1,13 @@
+@echo off
+echo Lade .env Konfiguration...
+
+REM .env Datei einlesen
+for /f "usebackq tokens=1,* delims==" %%A in ("C:\Users\mquas\Downloads\vaadin-oracle-user-management-sql\src\main\resources\.env") do (
+    set %%A=%%B
+)
+
+echo Starte Application...
+
+C:\Java\jdk22\bin\java -Xms512m -Xmx2g -jar target\vaadin-oracle-user-management-sql-0.0.1-SNAPSHOT.jar
+
+pause
